a0e10127574c9c410c7030799da4ac4de281b61c
lan-e <neladuranec@gmail.com>
2022-11-13 19:11:29 +0100

    changed Press Start 2P font

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,19 +1,19 @@
-import React from 'react'
+import React from "react";
 
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import HeroImg2 from '../components/HeroImg2';
-import Form from '../components/Form';
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import HeroImg2 from "../components/HeroImg2";
+import Form from "../components/Form";
 
 const Contact = () => {
   return (
     <div>
-      <Navbar/>
-      <HeroImg2 naslov="KONTAKT" text="Kontaktirajte me preko obrasca ili na telefon!" />
-      <Form/>
-      <Footer/>
+      <Navbar />
+      <HeroImg2 naslov="KONTAKT" />
+      <Form />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
